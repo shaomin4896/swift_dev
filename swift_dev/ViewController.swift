8270638a1b185e3fbd51bbfe1bb93d69305ac855
shaomin4896 <sam880819@gmail.com>
2020-01-17 22:05:15 +0800

    #1

--- swift_dev/ViewController.swift
+++ swift_dev/ViewController.swift
@@ -14,8 +14,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-    //test mother fucker
+    @IBAction func showMessage(sender: UIButton) {
+        let selectedButton = sender
+
+        // 從所選按鈕的標題標籤取得表情符號
+        if let wordToLookup = selectedButton.titleLabel?.text {
+
+            // 從字典取得表情符號的意義
+            // 程式碼填入至下方
+            let meaning = wordToLookup
+
+            // 變更以下這行程式將 Hello World 的顯示以來表情符號的意義來取代
+            let alertController = UIAlertController(title: "Meaning", message: meaning, preferredStyle: UIAlertController.Style.alert)
+
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+    }
 
 }
 
